9ad9d0677768607291c51195bb9e23837850c38e
hocht <rodrigoestillao@gmail.com>
2020-04-29 16:26:01 -0500

    Modificando por primera vez desde la Nespi

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,7 +1,9 @@
 print ("HolaMundo/")
 import sys
-print(sys.version)
+print (sys.version)
 print ("tu puta no me sirve")
 print ("tu hermana es una bolsa de semen")
-print("como subimos esto")
-print("hacemos el amor?")
+print ("como subimos esto")
+print ("hacemos el amor?")
+print ("Quiero una perra para preñarla")
+
