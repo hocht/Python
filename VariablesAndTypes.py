6de9fee0f8c47b72725bebd5e0346adb734c3538
Rodrigo Estilla <rodrigoestillao@gmail.com>
2020-05-01 14:53:16 -0500

    tercera practica

---
+++ VariablesAndTypes.py
@@ -0,0 +1,15 @@
+# Ejercicio
+micadena = "hola"
+
+miflotante = 10.0
+
+miIntegral = 20
+
+if micadena == "hola":
+	print("Cadena: %s" % micadena)
+
+if isinstance(miflotante, float) and miflotante == 10.0:
+	print ("flotante: %d" % miflotante)		
+
+if isinstance(miIntegral,int) and miIntegral == 20:
+	print ("Integral: %d" % miIntegral)
