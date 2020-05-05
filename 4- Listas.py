ae6f883c5a67298142f02a13a100b24249985b71
Rodrigo Estilla <rodrigoestillao@gmail.com>
2020-05-04 20:14:33 -0500

    Ejercicio de listas

---
+++ 4- Listas.py
@@ -0,0 +1,18 @@
+milista = []
+milista.append(1)
+milista.append(2)
+milista.append(3)
+print (milista[0]) # imprime 1
+print (milista[1]) # imprime 2 
+print (milista[2]) # imprime 3
+
+
+mylist = [1,2,3]
+
+# prints out 1,2,3
+for x in mylist:
+	print (x)
+
+cadenas = ['Hola','Tu mama']
+
+print(cadenas[1])
