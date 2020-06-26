7ba81fe90ffe297b5321c55eaf753e336329762a
Rodrigo Estilla <rodrigoestillao@gmail.com>
2020-06-26 16:23:10 -0500

    Repaso python basics

--- 3- VariablesAndTypes.py
+++ 3- VariablesAndTypes.py
@@ -1,15 +1,19 @@
 # Ejercicio
-micadena = "hola"
+micadena    = "hola"
 
-miflotante = 10.0
+miflotante  = 10.0
 
-miIntegral = 20
+miIntegral  = 20
 
 if micadena == "hola":
-	print("Cadena: %s" % micadena)
+print("Cadena: %s" % micadena)
 
 if isinstance(miflotante, float) and miflotante == 10.0:
-	print ("flotante: %d" % miflotante)		
+print ("flotante: %d" % miflotante)		
 
-if isinstance(miIntegral,int) and miIntegral == 20:
-	print ("Integral: %d" % miIntegral)
+if isinstance(miIntegral,int) and miIntegral    == 20:
+print ("Integral: %d" % miIntegral)
+
+print (micadena , miflotante   )
+
+
--- 4- Listas.py
+++ 4- Listas.py
@@ -7,7 +7,7 @@ print (milista[1]) # imprime 2
 print (milista[2]) # imprime 3
 
 
-mylist = [1,2,3]
+mylist = [1,2,3,4,5,6,7,8,9,10]
 
 # prints out 1,2,3
 for x in mylist:
@@ -16,3 +16,4 @@ for x in mylist:
 cadenas = ['Hola','Tu mama']
 
 print(cadenas[1])
+print(cadenas[0])
